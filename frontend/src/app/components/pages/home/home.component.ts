038e80ff6e29c40058a14175bca11b6fc14ba25c
nina <ninagbs@abv.bg>
2023-08-17 09:33:38 +0300

    added search component

--- frontend/src/app/components/pages/home/home.component.ts
+++ frontend/src/app/components/pages/home/home.component.ts
@@ -8,22 +8,22 @@ import { Food } from 'src/app/shared/models/Food';
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent implements OnInit{
-foods: Food[]=[];
-constructor(private foodService: FoodService, activatedRoute: ActivatedRoute){
-  // this we do about search
-  activatedRoute.params.subscribe((params)=>{
-    if(params.searchTerm){
-      // filter result base on the searchTerm
-      this.foods = this.foodService.getAllFoodBySearchTerm(params.searchTerm)
-    }else{
-      this.foods=foodService.getAll();
-    }
-  })
-  this.foods = foodService.getAll()
-}
+export class HomeComponent implements OnInit {
+  foods: Food[] = [];
+  constructor(private foodService: FoodService, activatedRoute: ActivatedRoute) {
+    // this we do about search
+    activatedRoute.params.subscribe((params) => {
+      if (params.searchTerm) {
+        // filter result base on the searchTerm
+        this.foods = this.foodService.getAllFoodBySearchTerm(params.searchTerm)
+      } else {
+        this.foods = foodService.getAll();
+      }
+    })
+    this.foods = foodService.getAll()
+  }
 
-ngOnInit(): void {
-  
-}
+  ngOnInit(): void {
+
+  }
 }
