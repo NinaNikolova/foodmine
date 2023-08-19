d595ceb4df0ab1d331ad65b6b22133bea66b4893
nina <ninagbs@abv.bg>
2023-08-19 13:48:10 +0300

    added login and logout views

--- frontend/src/app/services/user.service.ts
+++ frontend/src/app/services/user.service.ts
@@ -1,9 +1,51 @@
 import { Injectable } from '@angular/core';
+import { User } from '../shared/models/User';
+import { BehaviorSubject, Observable, tap } from 'rxjs';
+import { IUserLogin } from '../shared/interfaces/IUserLogin';
+import { HttpClient } from '@angular/common/http';
+import { USER_LOGIN_URL } from '../shared/constants/urls';
+import { ToastrService } from 'ngx-toastr';
+
+const USER_KEY = 'User';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
 
-  constructor() { }
+  private userSubject = new BehaviorSubject<User>(this.getUserFromLocalStorage());
+  public userObservable: Observable<User>;
+
+  constructor(private http: HttpClient, private toastrService: ToastrService) {
+    // userOservable is read-only version of userSunbject
+    this.userObservable = this.userSubject.asObservable();
+  }
+  login(userLogin: IUserLogin): Observable<User> {
+    return this.http.post<User>(USER_LOGIN_URL, userLogin).pipe(
+      tap({
+        next: (user) => {
+          this.setUserToLocalStorage(user);
+          this.userSubject.next(user);
+          this.toastrService.success(`Добре дошли във Злоядковци, ${user.name}`, 'Успешно влезнахте в профила си')
+        },
+        error: (errorResponse) => {
+          this.toastrService.error(errorResponse.error, 'Неуспешен опит за влизане в профила!')
+        }
+      })
+    )
+
+  }
+  logout(){
+    this.userSubject.next(new User());
+    localStorage.removeItem(USER_KEY);
+    window.location.reload();
+  }
+  private setUserToLocalStorage(user: User){
+    localStorage.setItem(USER_KEY, JSON.stringify(user))
+  }
+  private getUserFromLocalStorage():User{
+    const userJson = localStorage.getItem(USER_KEY);
+    if(userJson) return JSON.parse(userJson) as User;
+    return new User()
+  }
 }
---
+++ frontend/src/app/shared/interfaces/IUserLogin.ts
@@ -0,0 +1,5 @@
+export interface IUserLogin{
+    // by default all the fields in side the interface are required
+    email:string;
+    password:string;
+}
